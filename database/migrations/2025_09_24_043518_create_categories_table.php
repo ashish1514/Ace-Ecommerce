d942d77eab226017563eb4224ddd76b1d980031f
ashish1514 <ashish.acewebx@com>
2025-11-07 15:17:31 +0530

    --

--- database/migrations/2025_09_24_043518_create_categories_table.php
+++ database/migrations/2025_09_24_043518_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-             $table->text('image');
+            $table->text('image');
             $table->text('description')->nullable();
             $table->string('status')->default('Active');
             $table->timestamps();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionsSeeder::class, 
             UsersSeeder::class,        
-            SettingSeeder::class,
         ]);
     }
 }
